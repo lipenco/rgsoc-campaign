45b1d2091705fa1f3b7210c3565a9dbbe3229abd
Magdalena Lipinska <mag.lipinska@gmail.com>
2014-01-22 11:45:14 -0800

    design adjustment

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,7 +2,7 @@
 #   require 'devise/orm/active_record'
 #
 #   c.http_authenticatable = true
-#   c.mailer_sender = 'summer-of-code@railsgirls.com'
+#   c.mailer_sender = 'mag.lipinska@gmail.com'
 #
 #   # settings = Rails.application.config.settings
 #   # c.omniauth :github, settings.github.client_id, settings.github.client_secret, :scope => settings.github.scope
