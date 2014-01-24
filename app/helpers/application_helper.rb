c44078017f29162fcadb3cd4617cb6689ad380d5
Magdalena Lipinska <mag.lipinska@gmail.com>
2014-01-24 12:02:40 -0800

    fixing bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def format_donation(package)
     html = "<strong>#{number_to_currency(donation.amount_in_dollars)}</strong>"
-    html = "#{html} (Package: #{donation.package})" unless donation.package == 'Custom'
+    html = "#{html}" unless donation.package == 'Custom'
     html.html_safe
     
   end
